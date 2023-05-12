42ea811541529461ddd28a393230ca1f4c1b1ae1
IlliaKit <kitilla7@gmail.com>
2023-05-12 21:26:00 +0200

    edit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ window.addEventListener("scroll", function () {
   mountains_behind.style.right = value * 0.5 + "px";
 
   mountains_front.style.top = value * 0 + "px";
-  text.style.marginRight = value * 5 + "px";
+  text.style.marginRight = value * 2.5 + "px";
 
   btn.style.marginTop = value * 1.5 + "px";
   header.style.top = value * 0.5 + "px";
